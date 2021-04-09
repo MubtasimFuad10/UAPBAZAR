e9bb4988b9a2d815fcef1e157144316a7de76d8e
MFT10 <53475627+MFT10@users.noreply.github.com>
2021-04-09 18:37:04 +0600

    'SceneSwitcher'

---
+++ src/sample/SceneSwitcher.java
@@ -0,0 +1,51 @@
+package sample;
+
+import ClassWorks.View;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SceneSwitcher {
+    private static Map<ClassWorks.View, Parent> cache = new HashMap<>();
+
+    private static Scene scene;
+
+
+    public static void setScene(Scene scene) {
+
+        ClassWorks.SceneSwitcher.scene = scene;
+    }
+    public static void switchTo(View view) {
+        if (scene == null) {
+            System.out.println("No scene was set");
+            return;
+        }
+
+        try {
+            Parent root;
+
+            if (cache.containsKey(view)) {
+                System.out.println("Loading from cache");
+
+                root = cache.get(view);
+            } else {
+                System.out.println("Loading from FXML");
+
+                root = FXMLLoader.load(
+                        ClassWorks.SceneSwitcher.class.getResource(view.getFileName())
+                );
+
+                cache.put(view,root);
+            }
+
+            scene.setRoot(root);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
