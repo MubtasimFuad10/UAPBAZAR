17eb8c749cd52a42773fd3099c4dd741df007d00
Rohid <rohid.personal@gmail.com>
2021-04-11 02:03:41 +0600

    List view

--- UAPBazaar/src/sample/Main.java
+++ UAPBazaar/src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.scene.control.ListView;
 import sample.SceneSwitcher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,8 +9,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     public static void main(String[] args) {
         launch(args);
+
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
